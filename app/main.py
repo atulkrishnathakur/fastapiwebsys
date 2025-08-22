b0592d83f137da9756314330804f535282ec3b53
Atul <gitvcs@gmail.com>
2025-08-22 11:07:18 +0530

    url create

--- app/main.py
+++ app/main.py
@@ -29,8 +29,15 @@ engine = create_engine("mysql+pymysql://root:123456789@mysqlcontainer:3306/websy
 metadata = MetaData()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# DeclarativeBase used in sqlalchemy 2.0 
 
+# general function
+def none_to_empty(value):
+    if value is None:
+        return ""
+    return value
+
+
+# DeclarativeBase used in sqlalchemy 2.0 
 class Base(DeclarativeBase):
     pass
 
@@ -106,6 +113,42 @@ class VerticalMenuUrlMaster(Base):
     meta_tag_description: Mapped[String] = mapped_column('meta_tag_description',String(255),nullable=True)
     course_group_id: Mapped[BigInteger] = mapped_column('course_group_id',BigInteger,ForeignKey('course_group_master.id'),nullable=True)
 
+
+class CourseChapterDataDtl(Base):
+    __tablename__ = 'course_chapter_data_dtl'
+    __table_args__ = {"mysql_engine": "InnoDB"}
+    
+    id: Mapped[BigInteger] = mapped_column('id',BigInteger, primary_key=True, nullable=False, autoincrement=True)
+    course_id: Mapped[BigInteger] = mapped_column('course_id',BigInteger,ForeignKey('course_master.id'),nullable=True)
+    vertical_menu_id: Mapped[BigInteger] = mapped_column('vertical_menu_id',BigInteger,ForeignKey('vertical_menu_url_master.id'),nullable=True)
+    chapter_block_heading: Mapped[String] = mapped_column('chapter_block_heading',String(255),nullable=True)
+    paragraph1: Mapped[String] = mapped_column('paragraph1',String(1200),nullable=True)
+    paragraph2: Mapped[String] = mapped_column('paragraph2',String(1200),nullable=True)
+    paragraph3: Mapped[String] = mapped_column('paragraph3',String(1200),nullable=True)
+    paragraph4: Mapped[String] = mapped_column('paragraph4',String(1200),nullable=True)
+    paragraph5: Mapped[String] = mapped_column('paragraph5',String(1200),nullable=True)
+    syntax: Mapped[String] = mapped_column('syntax',String(1200),nullable=True)
+    syntax_example: Mapped[String] = mapped_column('syntax_example',String(1200),nullable=True)
+    program_code: Mapped[Text] = mapped_column('program_code',Text,nullable=True)
+    program_output: Mapped[Text] = mapped_column('program_output',Text,nullable=True)
+    program_algorithm: Mapped[Text] = mapped_column('program_algorithm',Text,nullable=True)
+    note1: Mapped[String] = mapped_column('note1',String(255),nullable=True)
+    note2: Mapped[String] = mapped_column('note2',String(255),nullable=True)
+    note3: Mapped[String] = mapped_column('note3',String(255),nullable=True)
+    note4: Mapped[String] = mapped_column('note4',String(255),nullable=True)
+    note5: Mapped[String] = mapped_column('note5',String(255),nullable=True)
+    img1: Mapped[String] = mapped_column('img1',String(255),nullable=True)
+    img2: Mapped[String] = mapped_column('img2',String(255),nullable=True)
+    img3: Mapped[String] = mapped_column('img3',String(255),nullable=True)
+    img4: Mapped[String] = mapped_column('img4',String(255),nullable=True)
+    img5: Mapped[String] = mapped_column('img5',String(255),nullable=True)
+    status: Mapped[String] = mapped_column('status',String(1),nullable=True,default=1,comment="1=Active,0=Inactive")
+    created_at: Mapped[DateTime] = mapped_column('created_at',DateTime, nullable=True, server_default=func.now())
+    updated_at: Mapped[DateTime] = mapped_column('updated_at',DateTime,nullable=True)
+    created_by: Mapped[BigInteger] = mapped_column('created_by',BigInteger,nullable=True)
+    updated_by: Mapped[BigInteger] = mapped_column('updated_by',BigInteger,nullable=True)
+    is_try_yourself: Mapped[String] = mapped_column('is_try_yourself',String(1),nullable=True)
+    
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
@@ -241,31 +284,41 @@ async def get_vertical_menu_url(request: Request):
     try:
         dbsession = SessionLocal()
         http_status_code = status.HTTP_200_OK
-        stmt = select(VerticalMenuUrlMaster).order_by(VerticalMenuUrlMaster.id)
-        result = dbsession.execute(stmt).scalars().all()
+        # outerjoin() used for left join
+        stmt = (
+            select(VerticalMenuUrlMaster,VerticalMenuGroupMaster,CourseMaster,CourseGroupMaster)
+            .outerjoin(VerticalMenuGroupMaster,VerticalMenuUrlMaster.vertical_menu_group_id==VerticalMenuGroupMaster.id)
+            .outerjoin(CourseMaster,VerticalMenuUrlMaster.course_id==CourseMaster.id)
+            .outerjoin(CourseGroupMaster,VerticalMenuUrlMaster.course_group_id==CourseGroupMaster.id)
+            .order_by(VerticalMenuUrlMaster.id)
+            )
+        result = dbsession.execute(stmt).all()
         datalist = list()
-        for rowobj in result:
+        for vmenu,vmenugroup,course,coursegroup in result:
             datadict = {}
-            datadict['id'] = rowobj.id
-            datadict['menu_order_by'] = rowobj.menu_order_by
-            datadict['route_name'] = rowobj.route_name
-            datadict['course_id'] = rowobj.course_id
-            datadict['vertical_menu_group_id'] = rowobj.vertical_menu_group_id
-            datadict['status'] = rowobj.status
+            datadict['id'] = vmenu.id
+            datadict['menu_order_by'] = vmenu.menu_order_by
+            datadict['route_name'] = none_to_empty(vmenu.route_name)
+            datadict['course_id'] = vmenu.course_id
+            datadict['course_name'] = course.course_name
+            datadict['vertical_menu_group_id'] = vmenu.vertical_menu_group_id
+            datadict['vertical_menu_group_name'] = vmenugroup.group_name
+            datadict['status'] = vmenu.status
             updatedatdate = ""
-            if rowobj.updated_at is not None:
-                updatedatdate = rowobj.updated_at.strftime("%d-%m-%Y")
+            if vmenu.updated_at is not None:
+                updatedatdate = vmenu.updated_at.strftime("%d-%m-%Y")
             
-            datadict['updated_at'] = updatedatdate
-            datadict['chapter_heading_main'] = rowobj.chapter_heading_main
-            datadict['menu_name'] = rowobj.menu_name
-            datadict['url_slug'] = rowobj.url_slug
-            datadict['is_course_home'] = rowobj.is_course_home
-            datadict['title_tag'] = rowobj.title_tag
-            datadict['meta_tag_keywords'] = rowobj.meta_tag_keywords
-            datadict['meta_tag_description'] = rowobj.meta_tag_description
-            datadict['hidden_ranking_contents'] = rowobj.hidden_ranking_contents
-            datadict['course_group_id'] = rowobj.course_group_id
+            datadict['updated_at'] = none_to_empty(updatedatdate)
+            datadict['chapter_heading_main'] = vmenu.chapter_heading_main
+            datadict['menu_name'] = vmenu.menu_name
+            datadict['url_slug'] = vmenu.url_slug
+            datadict['is_course_home'] = vmenu.is_course_home
+            datadict['title_tag'] = vmenu.title_tag
+            datadict['meta_tag_keywords'] = vmenu.meta_tag_keywords
+            datadict['meta_tag_description'] = vmenu.meta_tag_description
+            datadict['hidden_ranking_contents'] = vmenu.hidden_ranking_contents
+            datadict['course_group_id'] = vmenu.course_group_id
+            datadict['course_group_name'] = coursegroup.course_group_name
 
             datalist.append(datadict)
             
@@ -285,3 +338,77 @@ async def get_vertical_menu_url(request: Request):
         }
         response = JSONResponse(content=data,status_code=http_status_code)
         return response
+
+@app.get("/all-chapters",name="allchapters")
+async def get_all_chapter_data(request: Request):
+    try:
+        dbsession = SessionLocal()
+        http_status_code = status.HTTP_200_OK
+        # outerjoin() used for left join
+        baseUrl = request.base_url._url
+        stmt = (
+            select(CourseChapterDataDtl,CourseMaster,VerticalMenuUrlMaster)
+            .outerjoin(CourseMaster,CourseChapterDataDtl.course_id==CourseMaster.id)
+            .outerjoin(VerticalMenuUrlMaster,CourseChapterDataDtl.vertical_menu_id==VerticalMenuUrlMaster.id)
+            .order_by(CourseChapterDataDtl.id)
+            )
+        result = dbsession.execute(stmt).all()
+        datalist = list()
+        for courseChapDtl,course,vmenu in result:
+            datadict = {}
+            datadict['id'] = courseChapDtl.id
+            datadict['course_id'] = courseChapDtl.course_id
+            datadict['course_name'] = course.course_name
+            datadict['vertical_menu_id'] = courseChapDtl.vertical_menu_id
+            datadict['menu_name'] = vmenu.menu_name
+            datadict['chapter_block_heading'] = courseChapDtl.chapter_block_heading
+            datadict['paragraph1'] = courseChapDtl.paragraph1
+            datadict['paragraph2'] = courseChapDtl.paragraph2
+            datadict['paragraph3'] = courseChapDtl.paragraph3
+            datadict['paragraph4'] = courseChapDtl.paragraph4
+            datadict['paragraph5'] = courseChapDtl.paragraph5
+            datadict['syntax'] = courseChapDtl.syntax
+            datadict['syntax_example'] = courseChapDtl.syntax_example
+            
+            datadict['program_code'] = courseChapDtl.program_code
+            datadict['program_output'] = courseChapDtl.program_output
+            datadict['program_algorithm'] = courseChapDtl.program_algorithm
+            datadict['note1'] = courseChapDtl.note1
+            datadict['note2'] = courseChapDtl.note2
+            datadict['note3'] = courseChapDtl.note3
+            datadict['note4'] = courseChapDtl.note4
+            datadict['note5'] = courseChapDtl.note5
+            img1_url = ""
+            if courseChapDtl.img1 is not None:
+                img1_url = f"{baseUrl}static/uploads/{courseChapDtl.img1}"
+            datadict['img1'] = img1_url
+            datadict['img2'] = courseChapDtl.img2
+            datadict['img3'] = courseChapDtl.img3
+            datadict['img4'] = courseChapDtl.img4
+            datadict['img5'] = courseChapDtl.img5
+            datadict['status'] = courseChapDtl.status
+            updatedatdate = ""
+            if courseChapDtl.updated_at is not None:
+                updatedatdate = courseChapDtl.updated_at.strftime("%d-%m-%Y")
+            
+            datadict['updated_at'] = none_to_empty(updatedatdate)
+            datadict['is_try_yourself'] = courseChapDtl.is_try_yourself
+            datalist.append(datadict)
+            
+        response_dict = {
+            "status_code": http_status_code,
+            "status":True,
+            "message":"all chapter data menu",
+            "data":datalist
+        }
+        return response_dict
+    except Exception as e:
+        http_status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        data = {
+            "status_code": http_status_code,
+            "status":False,
+            "message":str(e)
+        }
+        response = JSONResponse(content=data,status_code=http_status_code)
+        return response
+
